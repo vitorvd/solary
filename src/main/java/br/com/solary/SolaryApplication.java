836f1cae76fd63859198b56efdcd6fd6965796bd
Vitor Versiani <vitordversiani@gmail.com>
2022-05-23 10:19:37 -0300

    commit initial

---
+++ src/main/java/br/com/solary/SolaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.solary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solary/SolaryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
